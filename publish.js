4e0c12b5a3191e6807ff72903be132b86fb1d29f
Ihor Dubii <ihor.dubii@unibrix.com>
2023-01-17 16:31:11 +0200

    Publish script

--- publish.js
+++ publish.js
@@ -1,15 +1,20 @@
 const shell = require('shelljs')
 
 const dir = 'publish';
-const sourcePath = 'dist/JSWrapper.bundle.js'
+const sourcePaths = ['dist/*', 'index.html']
 const branch = `js/build_${(new Date()).toLocaleDateString('en-UK').split('/').reverse().join('-')}`
 
 const sdkConfigs = [
     {
-        destinationPath: 'BladeSDK/src/main/assets',
+        sdkName: 'kotlin',
         repoUrl: 'git@github.com:Blade-Labs/kotlin-blade.git',
-        sdkName: 'kotlin'
-    }
+        destinationPath: 'BladeSDK/src/main/assets',
+    },
+    {
+        sdkName: 'swift',
+        repoUrl: 'git@github.com:Blade-Labs/swift-blade.git',
+        destinationPath: 'Sources/SwiftBlade/JS',
+    },
 ];
 
 
@@ -23,56 +28,13 @@ for (const {destinationPath, repoUrl, sdkName} of sdkConfigs) {
     shell.exec(`git clone ${repoUrl} ${dir}/${sdkName}`);
     shell.cd(`${dir}/${sdkName}`);
     shell.exec(`git checkout -b ${branch}`);
-    shell.exec(`git pull origin ${branch}`);
-    shell.cp(`../../${sourcePath}`, destinationPath);
+
+    for (const sourcePath of sourcePaths) {
+        shell.cp(`../../${sourcePath}`, destinationPath);
+    }
     shell.exec(`git add ${destinationPath}`);
     shell.exec(`git commit -m "build JS from ${Date().toString()}"`);
-    shell.exec(`git push --set-upstream origin ${branch}`);
-    shell.rm('-rf', `../../${dir}/${sdkName}`);
-
-}
-
-
-
-
-
-// shell.rm('-rf', `${dir}/${sdkName}`);
-
-
-//
-// (async () => {
-//     // console.log(await execShellCommand('git clone git@github.com:Blade-Labs/kotlin-blade.git'));
-//     try {
-//         console.log(await execShellCommand('pwd'));
-//         console.log(shell.exec('pwd'))
-//         console.log(await execShellCommand(`rm -rf ${dir}/${sdkName}`));
-//
-//         console.log(await execShellCommand(`git clone ${repoUrl} ${dir}/${sdkName}`));
-//         console.log(await execShellCommand('ls'));
-//         console.log(await execShellCommand('pwd'));
-//     } catch (e) {
-//         console.log("Catch exception", e);
-//     }
-//
-// })();
-
-
-function execShellCommand(cmd) {
-    const exec = require('child_process').exec;
-    return new Promise((resolve, reject) => {
-        exec(cmd, (error, stdout, stderr) => {
-            console.log('error', error);
-            console.log('stdout', stdout);
-            console.log('stderr', stderr);
-
-            if (error) {
-                console.warn(error);
-                throw error;
-            }
-            if (stderr) {
-                // return reject(stderr)
-            }
-            resolve(stdout);
-        });
-    });
+    shell.exec(`git push --set-upstream origin ${branch} --force`);
+    shell.cd('../..');
+    shell.rm('-rf', `${dir}/${sdkName}`);
 }
