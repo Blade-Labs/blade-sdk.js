1034b4f2139cf9520ab502d8227f794faa7abc02
Ihor Dubii <ihor.dubii@unibrix.com>
2023-06-20 18:34:00 +0300

    fix tests

--- test/src/index.test.ts
+++ test/src/index.test.ts
@@ -5,14 +5,21 @@ import {Network} from "../../src/models/Networks";
 import {isEqual} from "lodash";
 import {Buffer} from "buffer";
 import config from "../../src/config";
-
-import {BladeSDK, ParametersBuilder} from "../../src/webView"
-import dotenv from "dotenv"
-import fetch from "node-fetch"
-
+const {BladeSDK, ParametersBuilder} = require("../../src/webView");
+import dotenv from "dotenv";
+import fetch from"node-fetch";
 import {PrivateKey} from "@hashgraph/sdk";
 import {hethers} from "@hashgraph/hethers";
+import { TextEncoder, TextDecoder } from 'util';
+import crypto from "crypto";
 
+Object.defineProperty(global.self, "crypto", {
+    value: {
+        subtle: crypto.webcrypto.subtle,
+    },
+});
+
+Object.assign(global, { TextDecoder, TextEncoder, fetch });
 
 dotenv.config();
 const bladeSdk = new BladeSDK(true);
@@ -24,20 +31,9 @@ const accountId = process.env.ACCOUNT_ID;
 const privateKey2 = process.env.PRIVATE_KEY2; // ECDSA
 const accountId2 = process.env.ACCOUNT_ID2;
 
-import { TextEncoder, TextDecoder } from 'util';
-import crypto from "crypto";
-
-Object.defineProperty(global.self, "crypto", {
-    value: {
-        subtle: crypto.webcrypto.subtle,
-    },
-});
-
-Object.assign(global, { TextDecoder, TextEncoder });
-Object.assign(global, { fetch });
 
 test('bladeSdk defined', () => {
-    expect(window["bladeSdk"]).toBeDefined()
+    expect(window.bladeSdk).toBeDefined()
 });
 
 test('bladeSdk.init', async () => {
@@ -179,7 +175,7 @@ test('bladeSdk.contractCallFunction', async () => {
     checkResult(result, false);
     expect(result.error.reason.includes("Unexpected token")).toEqual(true);
 
-    //fail on low gas
+    // fail on low gas
     params = new ParametersBuilder().addString(message).addTuple(new ParametersBuilder().addUInt64(num1).addUInt64(num2));
     result = await bladeSdk.contractCallFunction(contractId, "set_message", params, accountId, privateKey, 1, false, completionKey);
     checkResult(result, false);
@@ -325,7 +321,7 @@ test('bladeSdk.createAccount', async () => {
 }, 60_000);
 
 test('bladeSdk.getAccountInfo', async () => {
-    let result = await bladeSdk.init(process.env.API_KEY, process.env.NETWORK, process.env.DAPP_CODE, process.env.VISITOR_ID, process.env.SDK_ENV, sdkVersion, completionKey);
+    const result = await bladeSdk.init(process.env.API_KEY, process.env.NETWORK, process.env.DAPP_CODE, process.env.VISITOR_ID, process.env.SDK_ENV, sdkVersion, completionKey);
     checkResult(result);
 
     const account = await bladeSdk.createAccount("device-id", completionKey);
@@ -424,7 +420,7 @@ test('bladeSdk.sign + signVerify', async () => {
         Buffer.from(result.data.signedMessage, "hex")
     )).toEqual(true);
 
-    //invalid private key
+    // invalid private key
     let resultInvalid = await bladeSdk.sign(messageString, `privateKey`, completionKey);
     checkResult(resultInvalid, false);
 
@@ -511,7 +507,7 @@ test('bladeSdk.getTransactions', async () => {
     const transactionId = result.data.transactionId.replace("@", ".");
     await sleep(10_000);
 
-    //get expected transaction
+    // get expected transaction
     result = await bladeSdk.getTransactions(accountId, "", "", "5", completionKey);
     checkResult(result);
 
@@ -542,7 +538,7 @@ test('bladeSdk.getTransactions', async () => {
     expect(latestTransaction.type).toEqual("CRYPTOTRANSFER");
 
 
-    //next page
+    // next page
     result = await bladeSdk.getTransactions(accountId, "", nextPage, "5", completionKey);
     checkResult(result);
 
@@ -562,11 +558,11 @@ test('bladeSdk.getTransactions', async () => {
         expect(result.data.transactions[0].plainData).toHaveProperty("amount");
     }
 
-    //invalid accountId
+    // invalid accountId
     result = await bladeSdk.getTransactions('0.dgsgsdgdsgdsgdsg', "", "", "5", completionKey);
     checkResult(result, false);
 
-    //invalid tx
+    // invalid tx
     result = await getTransaction(Network.Testnet, "wrong tx id", accountId);
     expect(Array.isArray(result));
     expect(result.length).toEqual(0)
@@ -617,9 +613,9 @@ test('ParametersBuilder.defined', async () => {
 
 test('ParametersBuilder.complicatedCheck', async () => {
     const paramsEncodedExample = 'W3sidHlwZSI6InN0cmluZyIsInZhbHVlIjpbIkhlbGxvLCBCYWNrZW5kIl19LHsidHlwZSI6ImJ5dGVzMzIiLCJ2YWx1ZSI6WyJXekFzTVN3eUxETXNOQ3cxTERZc055dzRMRGtzTVRBc01URXNNVElzTVRNc01UUXNNVFVzTVRZc01UY3NNVGdzTVRrc01qQXNNakVzTWpJc01qTXNNalFzTWpVc01qWXNNamNzTWpnc01qa3NNekFzTXpGZCJdfSx7InR5cGUiOiJhZGRyZXNzW10iLCJ2YWx1ZSI6WyIwLjAuNDg3Mzg1MzkiLCIwLjAuNDg3Mzg1MzgiLCIwLjAuNDg3Mzg1MzciXX0seyJ0eXBlIjoiYWRkcmVzcyIsInZhbHVlIjpbIjAuMC40ODg1MDQ2NiJdfSx7InR5cGUiOiJhZGRyZXNzIiwidmFsdWUiOlsiMC4wLjQ5OTMyNiJdfSx7InR5cGUiOiJhZGRyZXNzIiwidmFsdWUiOlsiMC4wLjQ4ODAxNjg4Il19LHsidHlwZSI6ImludDY0IiwidmFsdWUiOlsiMSJdfSx7InR5cGUiOiJ1aW50OCIsInZhbHVlIjpbIjEyMyJdfSx7InR5cGUiOiJ1aW50NjRbXSIsInZhbHVlIjpbIjEiLCIyIiwiMyJdfSx7InR5cGUiOiJ1aW50MjU2W10iLCJ2YWx1ZSI6WyIxIiwiMiIsIjMiXX0seyJ0eXBlIjoidHVwbGUiLCJ2YWx1ZSI6WyJXM3NpZEhsd1pTSTZJbWx1ZERZMElpd2lkbUZzZFdVaU9sc2lOU0pkZlN4N0luUjVjR1VpT2lKcGJuUTJOQ0lzSW5aaGJIVmxJanBiSWpFd0lsMTlYUT09Il19LHsidHlwZSI6InR1cGxlIiwidmFsdWUiOlsiVzNzaWRIbHdaU0k2SW1sdWREWTBJaXdpZG1Gc2RXVWlPbHNpTlRBaVhYMHNleUowZVhCbElqb2lkSFZ3YkdWYlhTSXNJblpoYkhWbElqcGJJbGN6YzJsa1NHeDNXbE5KTmtsdGJIVmtSRmt3U1dsM2FXUnRSbk5rVjFWcFQyeHphVTFVV1dsWVdEQnpaWGxLTUdWWVFteEphbTlwWVZjMU1FNXFVV2xNUTBveVdWZDRNVnBUU1RaWGVVbDZUV2xLWkdaV01EMGlMQ0pYTTNOcFpFaHNkMXBUU1RaSmJXeDFaRVJaTUVscGQybGtiVVp6WkZkVmFVOXNjMmxPVTBwa1psTjROMGx1VWpWalIxVnBUMmxLY0dKdVVUSk9RMGx6U1c1YWFHSklWbXhKYW5CaVNXcEZkMGxzTVRsWVVUMDlJbDE5WFE9PSJdfSx7InR5cGUiOiJ0dXBsZVtdIiwidmFsdWUiOlsiVzNzaWRIbHdaU0k2SW1sdWREWTBJaXdpZG1Gc2RXVWlPbHNpTVRZaVhYMHNleUowZVhCbElqb2lhVzUwTmpRaUxDSjJZV3gxWlNJNld5SXpNaUpkZlYwPSIsIlczc2lkSGx3WlNJNkltbHVkRFkwSWl3aWRtRnNkV1VpT2xzaU5TSmRmU3g3SW5SNWNHVWlPaUpwYm5RMk5DSXNJblpoYkhWbElqcGJJakV3SWwxOVhRPT0iXX0seyJ0eXBlIjoidHVwbGVbXSIsInZhbHVlIjpbIlczc2lkSGx3WlNJNkltbHVkRFkwSWl3aWRtRnNkV1VpT2xzaU5UQWlYWDBzZXlKMGVYQmxJam9pZEhWd2JHVmJYU0lzSW5aaGJIVmxJanBiSWxjemMybGtTR3gzV2xOSk5rbHRiSFZrUkZrd1NXbDNhV1J0Um5Oa1YxVnBUMnh6YVUxVVdXbFlXREJ6WlhsS01HVllRbXhKYW05cFlWYzFNRTVxVVdsTVEwb3lXVmQ0TVZwVFNUWlhlVWw2VFdsS1pHWldNRDBpTENKWE0zTnBaRWhzZDFwVFNUWkpiV3gxWkVSWk1FbHBkMmxrYlVaelpGZFZhVTlzYzJsT1UwcGtabE40TjBsdVVqVmpSMVZwVDJsS2NHSnVVVEpPUTBselNXNWFhR0pJVm14SmFuQmlTV3BGZDBsc01UbFlVVDA5SWwxOVhRPT0iLCJXM3NpZEhsd1pTSTZJbWx1ZERZMElpd2lkbUZzZFdVaU9sc2lOVEFpWFgwc2V5SjBlWEJsSWpvaWRIVndiR1ZiWFNJc0luWmhiSFZsSWpwYklsY3pjMmxrU0d4M1dsTkpOa2x0YkhWa1JGa3dTV2wzYVdSdFJuTmtWMVZwVDJ4emFVMVVXV2xZV0RCelpYbEtNR1ZZUW14SmFtOXBZVmMxTUU1cVVXbE1RMG95V1ZkNE1WcFRTVFpYZVVsNlRXbEtaR1pXTUQwaUxDSlhNM05wWkVoc2QxcFRTVFpKYld4MVpFUlpNRWxwZDJsa2JVWnpaRmRWYVU5c2MybE9VMHBrWmxONE4wbHVValZqUjFWcFQybEtjR0p1VVRKT1EwbHpTVzVhYUdKSVZteEphbkJpU1dwRmQwbHNNVGxZVVQwOUlsMTlYUT09Il19LHsidHlwZSI6ImFkZHJlc3MiLCJ2YWx1ZSI6WyIwLjAuMTIzNDUiXX0seyJ0eXBlIjoidWludDY0IiwidmFsdWUiOlsiNTY3ODQ2NDU2NDUiXX0seyJ0eXBlIjoidWludDI1NiIsInZhbHVlIjpbIjEyMzQ1Il19XQ==';
-    let tuple0 = new ParametersBuilder().addInt64(16).addInt64(32);
-    let tuple1 = new ParametersBuilder().addInt64(5).addInt64(10);
-    let tuple2 = new ParametersBuilder().addInt64(50).addTupleArray([tuple0, tuple1]);
+    const tuple0 = new ParametersBuilder().addInt64(16).addInt64(32);
+    const tuple1 = new ParametersBuilder().addInt64(5).addInt64(10);
+    const tuple2 = new ParametersBuilder().addInt64(50).addTupleArray([tuple0, tuple1]);
 
     const params = new ParametersBuilder()
         .addString("Hello, Backend")
